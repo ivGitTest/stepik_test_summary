dcae7156e4981506e9d53b639b0499fe6b94d4ce
Ivan Rybakov <regformonly@yandex.ru>
2023-06-11 20:14:32 +0300

    add code to get url next page to test success transfer to

--- test_main_page.py
+++ test_main_page.py
@@ -15,11 +15,6 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
-def test_guest_open_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
-    page.should_be_login_form()
-    page.should_be_register_form()
