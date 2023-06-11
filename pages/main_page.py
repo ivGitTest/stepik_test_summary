8c0b0dc7971e86ab1c620e8f9c37c6bd0bfaa5e0
Ivan Rybakov <regformonly@yandex.ru>
2023-06-11 18:18:48 +0300

    Add control exceptions

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,11 @@
-class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
 
-    def open(self):
-        self.browser.get(self.url)
+class MainPage(BasePage):
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not present"
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+
